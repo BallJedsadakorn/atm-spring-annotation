06f0bdfff3946289e6ade1821f6539907a2d1a81
BallJedsadakorn <jedsadakorn.boo@ku.th>
2023-08-08 18:39:27 +0700

    Add ATMConfig

--- src/main/java/atm/ATM.java
+++ src/main/java/atm/ATM.java
@@ -1,16 +1,15 @@
 package atm;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 /**
  * An ATM that accesses a bank.
  */
-@Component
 public class ATM {
-	@Autowired
 	private Bank bank;
 	private Customer loginCustomer;
+	public ATM(Bank bank) {
+		this.bank = bank;
+		this.loginCustomer = null;
+	}
 	/**
 	 * Finds customer in bank.
 	 * @param id current customer id
